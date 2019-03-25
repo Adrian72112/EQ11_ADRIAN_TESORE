0e2487a5c98e6a59f0ea8b27a2293024631d1214
Adrian Tesore <adrian72112@gmail.com>
2019-03-25 20:05:28 -0300

    Update ContadorPalabra.java

--- UT1/src/TA3/ContadorPalabra.java
+++ UT1/src/TA3/ContadorPalabra.java
@@ -5,6 +5,10 @@
  */
 package TA3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author adriantesore
@@ -29,10 +33,37 @@ public class ContadorPalabra {
         return contador;
     }
     
+    public Map<String, Integer> contarVolcalesConsonantes(String texto) {
+        ArrayList<Character> vocales = new ArrayList<Character>();
+        vocales.add('a');
+        vocales.add('e');
+        vocales.add('i');
+        vocales.add('o');
+        vocales.add('u');
+
+        int v = 0;
+        int c = 0;
+
+        for (int i = 0; i < texto.length(); i++) {
+            if (Character.isAlphabetic(c)) {
+                if (vocales.contains(texto.charAt(i)))
+                    v ++;
+            } else {
+                c ++;
+        }
+    }
+
+    Map<String, Integer> result = new HashMap<>();
+    result.put("vocales", v);
+    result.put("consonantes", c);
+
+    return result;
+}
+    
     public int cantidadLargoPalabra(int minLargo, String palabra){
         boolean esPalabra = false;
         int contador = 0 , largo = 0;
-        
+        char[] hola = {};
         for (char caracter: palabra.toCharArray()){
             if (Character.isAlphabetic(caracter)){
                 esPalabra = true;
